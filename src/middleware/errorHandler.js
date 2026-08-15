4dd72320eb1b6ddbc0f3a2d809192bce05b262f9
ab-e1 <abelghy17@gmail.com>
2026-08-15 22:14:56 +0300

    add error handler function and exported it

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,7 @@
+const errorHandler = (err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message || "internal server error",
+  });
+};
+
+module.exports = errorHandler;
