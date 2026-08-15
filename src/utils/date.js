f2da19791d7e3b340d5612c3614e02373015f875
ab-e1 <abelghy17@gmail.com>
2026-08-16 02:37:42 +0300

    fixed a runtome error

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,4 +1,4 @@
-const now = () => new Date.now().toISOString();
+const now = () => new Date().toISOString();
 const daysFromNow = (days) =>
   new Date(Date.now() + days * 24 * 60 * 60 * 1000).toISOString();
 
