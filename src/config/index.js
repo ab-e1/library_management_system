ed87df1ae2b791ad490f790dee5a5ff7341b13fc
ab-e1 <abelghy17@gmail.com>
2026-08-15 21:16:56 +0300

    included bycrypt for password hash , and initiated it in config.js

--- src/config/index.js
+++ src/config/index.js
@@ -3,4 +3,6 @@ require("dotenv").config();
 module.exports = {
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "JWT_SECRET",
+  jwtExpiresIn: "7d",
+  bcryptRounds: 10,
 };
