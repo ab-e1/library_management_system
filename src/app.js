e5114287e618926b143d53d8026725c3e8820993
ab-e1 <abelghy17@gmail.com>
2026-08-18 00:41:16 +0300

    installed and added swagger config in app.js

--- src/app.js
+++ src/app.js
@@ -4,11 +4,28 @@ const membersRoute = require("./routes/membersRoute.js");
 const loansRoute = require("./routes/loansRoute.js");
 const errorHandler = require("./middleware/errorHandler.js");
 const logger = require("./middleware/logger.js");
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 const app = express();
 app.use(express.json());
 
 app.use(logger);
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Library Management System",
+      version: "1.0.0",
+      description: "A library management API built with Express",
+    },
+  },
+  apis: ["./src/routes/*.js"],
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use("/api/books", booksRoute);
 app.use("/api/members", membersRoute);
