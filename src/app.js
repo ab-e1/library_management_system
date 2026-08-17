e34b52a20f14c494182ec7397e6a0edf82d70775
ab-e1 <abelghy17@gmail.com>
2026-08-18 00:00:11 +0300

    added the logger to app.js

--- src/app.js
+++ src/app.js
@@ -3,9 +3,12 @@ const booksRoute = require("./routes/booksRoute.js");
 const membersRoute = require("./routes/membersRoute.js");
 const loansRoute = require("./routes/loansRoute.js");
 const errorHandler = require("./middleware/errorHandler.js");
+const logger = require("./middleware/logger.js");
 
 const app = express();
 app.use(express.json());
+
+app.use(logger);
 
 app.use("/api/books", booksRoute);
 app.use("/api/members", membersRoute);
