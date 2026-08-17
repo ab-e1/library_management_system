325a391ce3431bf436eed1e3ac060d859d305365
ab-e1 <abelghy17@gmail.com>
2026-08-17 23:02:13 +0300

    chnged the response object to .ok , .error and .data to match with the others

--- src/utils/response.js
+++ src/utils/response.js
@@ -1,8 +1,8 @@
 const success = (res, data, statusCode = 200) => {
-  res.status(statusCode).json({ success: true, data });
+  res.status(statusCode).json({ ok: true, data });
 };
-const failure = (res, message, statusCode = 400) => {
-  res.status(statusCode).json({ success: false, message });
+const failure = (res, error, statusCode = 400) => {
+  res.status(statusCode).json({ ok: false, error });
 };
 
 module.exports = { success, failure };
