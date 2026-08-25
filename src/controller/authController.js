a93db84ab29f8bb41f8658b4ab81eed324f8fcff
ab-e1 <abelghy17@gmail.com>
2026-08-25 21:43:42 +0300

    added aync and wait to each fincton, and their respective call they make

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,16 +1,16 @@
 const authServices = require("../services/authService.js");
 const { success, failure } = require("../utils/response.js");
 
-const register = (req, res) => {
-  const result = authServices.register(req.body);
+const register = async (req, res) => {
+  const result = await authServices.register(req.body);
   if (!result.ok) {
     return failure(res, result.error, 400);
   }
   return success(res, { ...result.data, token: result.token });
 };
 
-const login = (req, res) => {
-  const result = authServices.login(req.body);
+const login = async (req, res) => {
+  const result = await authServices.login(req.body);
   if (!result.ok) {
     return failure(res, result.error, 400);
   }
