bca8560da7877251cc210808e2bccebc9000ccca
ab-e1 <abelghy17@gmail.com>
2026-08-25 18:30:38 +0300

    added teh sql tabels for the books, members, and loans for our db

---
+++ src/data/schema.sql
@@ -0,0 +1,34 @@
+/* create a book table */
+
+CREATE TABLE books (
+  id serial PRIMARY KEY,
+  title VARCHAR(100) NOT NULL,
+  author VARCHAR(50) NOT NULL,
+  genre VARCHAR(100),
+  year INTEGER,
+  copies INTEGER DEFAULT 1,
+  available_copies INTEGER DEFAULT 1
+);
+
+/* cretae a members table*/
+
+CREATE TABLE members (
+    id SERIAL PRIMARY KEY,
+    name  VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role varchar(50) DEFAULT 'member',
+    registered_at TIMESTAMP DEFAULT NOW()
+);
+
+/* create a loan table */
+ 
+ CREATE TABLE loans (
+    id SERIAL PRIMARY KEY,
+    book_id INTEGER REFERENCES books(id),
+    member_id INTEGER REFERENCES members(id),
+    borrowed_at TIMESTAMP,
+    due_at TIMESTAMP,
+    returned_at TIMESTAMP DEFAULT NULL,
+    status varchar(20) DEFAULT 'borrowed'
+);
