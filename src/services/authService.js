8ff93e15367702d4a8a7bf4c867158c3fea6875f
ab-e1 <abelghy17@gmail.com>
2026-08-25 21:42:47 +0300

    migrated the auth service to asyncronius, and added sql qurys for doing the job

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,35 +1,47 @@
 const bcrypt = require("bcrypt");
 const config = require("../config/index.js");
-const { signToken, verifyToken } = require("../utils/jwt.js");
-const { members } = require("../data/members.js");
-const memberService = require("../services/memberService.js");
+const { signToken } = require("../utils/jwt.js");
 const { memberInfo } = require("../utils/response.js");
+const pool = require("../data/connection.js");
 
-const register = (data) => {
-  const member = memberService.createMember(data);
+const register = async (data) => {
+  const duplicate = await pool.query(
+    "SELECT * FROM members WHERE email = $1 ",
+    [data.email],
+  );
 
-  if (!member.ok) return member;
-  const { password, ...memberWithoutPassowrd } = member.data;
+  if (duplicate.rows.length > 0) {
+    return { ok: false, error: "email already registered" };
+  }
+
+  const hashPassword = await bcrypt.hash(data.password, config.bcryptRounds);
+  const insert = await pool.query(
+    "INSERT INTO members (name, email, password, role, registered_at) VALUES($1, $2, $3, 'member', NOW()) RETURNING *",
+    [data.name, data.email, hashPassword],
+  );
+  const member = insert.rows[0];
+  const { password, ...memberWithoutPassowrd } = member;
   const token = signToken(memberWithoutPassowrd);
 
   return {
     ok: true,
-    data: memberInfo(member.data),
+    data: memberInfo(member),
     token: token,
   };
 };
 
-const login = (data) => {
-  const member = members.find(
-    (m) => m.email.trim().toLowerCase() === data.email.toLowerCase().trim(),
-  );
-  if (!member) {
+const login = async (data) => {
+  const duplicate = await pool.query("SELECT * FROM members WHERE email = $1", [
+    data.email,
+  ]);
+  if (duplicate.rows.length === 0) {
     return {
       ok: false,
       error: "no member with this email",
     };
   }
-  const match = bcrypt.compareSync(data.password, member.password);
+  const member = duplicate.rows[0];
+  const match = await bcrypt.compare(data.password, member.password);
   if (!match) {
     return {
       ok: false,
